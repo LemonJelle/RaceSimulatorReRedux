cbddb542c5a120392f53b8f60c5e2b2bea8b3473
Jelle <jelleplegt@gmail.com>
2023-12-17 20:46:39 +0100

    Level 2-3: Using the model

--- Model/IParticipant.cs
+++ Model/IParticipant.cs
@@ -12,7 +12,6 @@ namespace Model
         public int Points { get; set; }
         public IEquipment Equipment { get; set; }
         public TeamColors TeamColor { get; set; }
-
     }
 
     public enum TeamColors
