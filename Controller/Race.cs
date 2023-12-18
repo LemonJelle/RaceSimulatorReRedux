2bdface31a622866f9b004ca99f215654dbb5b4b
Jelle <jelleplegt@gmail.com>
2023-12-18 20:46:53 +0100

    4-3 Placing the participants

--- Controller/Race.cs
+++ Controller/Race.cs
@@ -18,13 +18,18 @@ namespace Controller
 
         public Race(Track track, List<IParticipant> participants)
         {
-            
+
             Track = track;
             Participants = participants;
             _random = new Random(DateTime.Now.Millisecond);
 
             //First check if amount of participants doesn't exceed the limit
             CheckAmountOfParticipants();
+
+            //Place participants on start and randomize equipment
+            GiveParticipantsStartPosition();
+            RandomizeEquipment();
+
         }
 
         public SectionData GetSectionData(Section section)
@@ -58,6 +63,56 @@ namespace Controller
             {
                 participant.Equipment.Quality = _random.Next();
                 participant.Equipment.Performance = _random.Next();
+            }
+        }
+
+        public void GiveParticipantsStartPosition()
+        {
+            //Make stack of start grid sections
+            Stack<Section> startGrids = new Stack<Section>();
+
+            //Search for startgrid sections in the track and add them to stack
+            foreach (Section section in Track.Sections)
+            {
+                if (section.SectionType == SectionTypes.StartGrid)
+                {
+                    startGrids.Push(section);
+                }
+            }
+
+            //Get number of participants
+            int participants = Participants.Count;
+
+            //Keep track of participants already placed, a counter of sorts
+            int participantsAlreadyPlaced = 0;
+
+            //Place participants as long as there is still a startgrid left
+            //Check how many participants are left
+            while (startGrids.Count > 0)
+            {
+                //Get section from stack
+                Section section = startGrids.Pop();
+                //Get data of each section
+                SectionData sectionData = GetSectionData(section);
+                //If there is still two or more participants left to place, take up two positions
+                if (participants - participantsAlreadyPlaced > 1)
+                {
+                    //Left
+                    sectionData.Left = Participants[participantsAlreadyPlaced];
+                    participantsAlreadyPlaced += 1;
+                    //Right
+                    sectionData.Right = Participants[participantsAlreadyPlaced];
+                    participantsAlreadyPlaced += 1;
+                    _positions[section] = sectionData;
+                }
+
+                //if there is still one participant left to place, take up the left position 
+                else if (participants - participants == 1)
+                {
+                    sectionData.Left = Participants[participants];
+                    participantsAlreadyPlaced += 1;
+                    _positions[section] = sectionData;
+                }
             }
         }
     }
