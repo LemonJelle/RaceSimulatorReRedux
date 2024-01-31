317bf992c3f9d1a6ede0c09cf63fcfa4d31d5850
Jelle <jelleplegt@gmail.com>
2024-01-31 21:23:39 +0100

    6-3 Loading and Caching

---
+++ RaceSimulatorWPFApp/ImageHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RaceSimulatorWPFApp
+{
+    //Creates, loads and edits images
+    public static class ImageHandler
+    {
+        private static Dictionary<string, Bitmap> _imageCache = new();
+
+        private static Bitmap GetBitmapImage(string path)
+        {
+            try
+            {
+                return _imageCache[path];
+            }
+            catch (KeyNotFoundException)
+            {
+                Bitmap image = new Bitmap(path);
+                _imageCache.Add(path, image);
+                return _imageCache[path];
+            }
+        }
+
+        private static void EmptyCache()
+        {
+            _imageCache.Clear();
+        }
+    }
+}
